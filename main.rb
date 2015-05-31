313e68aa2add79e09868837d853e3b48f104dfd5
danpaul <pt2323@gmail.com>
2015-05-31 18:05:14 -0400

    initial commit

---
+++ main.rb
@@ -0,0 +1,45 @@
+class Possible_states
+    @@default_state = false
+    def initialize(numberOfStateEllements)
+        @states = Array.new(numberOfStateEllements){|i| @@default_state }
+        @primary_pointer = -1
+        @secondary_pointer = numberOfStateEllements - 1;
+    end
+    def get_state(position)
+        return @states[position]
+    end
+    def get_states
+        return @states
+    end
+    def show_state
+        print(@states)
+        print("\n")
+    end
+    def next
+        @secondary_pointer += 1
+        if @secondary_pointer == @states.length
+            return self.primary_next()
+        else
+            @states[@secondary_pointer] = !@@default_state;
+            return true;
+        end
+    end
+    def primary_next
+        @primary_pointer += 1
+        if @primary_pointer == @states.length
+            return false;
+        end
+        # reset all values except primary positions
+        @states = @states.map{ |state| @@default_state }
+        @secondary_pointer = @primary_pointer
+        @states[@secondary_pointer] = !@@default_state
+        return true
+    end
+end
+
+possible_states = Possible_states.new(4);
+possible_states.show_state()
+
+while possible_states.next()
+    possible_states.show_state()
+end
---
+++ readme.md
@@ -0,0 +1,29 @@
+## About
+
+`all_possible_states` is a simple ruby class to iterate over every possible state of a binary array of a user defined length;
+
+## Example
+
+```ruby
+possible_states = Possible_states.new(4);
+possible_states.show_state()
+
+while possible_states.next()
+    possible_states.show_state()
+end
+```
+
+Output:
+```
+[false, false, false, false]
+[true, false, false, false]
+[true, true, false, false]
+[true, true, true, false]
+[true, true, true, true]
+[false, true, false, false]
+[false, true, true, false]
+[false, true, true, true]
+[false, false, true, false]
+[false, false, true, true]
+[false, false, false, true]
+```
